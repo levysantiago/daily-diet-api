d0628cee97827206373165dbcc86ffd3ca2a05c0
Levy Santiago <levyssantiago@gmail.com>
2023-08-17 23:09:34 -0300

    feat: create user route

---
+++ src/routes/users.ts
@@ -0,0 +1,33 @@
+import { randomUUID } from 'node:crypto'
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { knex } from '../database'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createUserBodySchema = z.object({
+      name: z.string(),
+      email: z.string().email(),
+    })
+
+    const { name, email } = createUserBodySchema.parse(request.body)
+
+    let sessionId = request.cookies.sessionId
+    if (!sessionId) {
+      sessionId = randomUUID()
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days,
+      })
+    }
+
+    await knex('users').insert({
+      id: randomUUID(),
+      name,
+      email,
+      session_id: sessionId,
+    })
+
+    return reply.status(201).send()
+  })
+}
